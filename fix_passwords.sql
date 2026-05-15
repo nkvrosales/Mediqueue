9f45743c5882d7877d727f8b968010b97484e6f6
Nicklaus Karl Vergel Rosales <154541225+nkvrosales@users.noreply.github.com>
2026-05-15 16:24:38 +0800

    Update

--- fix_passwords.sql
+++
@@ -1,3 +0,0 @@
-USE hospital_ticketing;
-UPDATE users SET password = '$2y$10$GOzKzx7SBbEugcuFWiZAke4VFoIlKHDF3W3pvfrzb9CZFDb.0RtA2' WHERE username = 'admin';
-UPDATE users SET password = '$2y$10$ZCAwae2pBzC8gjjEyxhO6uP7E2hPZHgXI6FJmciXGoO/zW8w5p6D6' WHERE username LIKE 'staff%';
